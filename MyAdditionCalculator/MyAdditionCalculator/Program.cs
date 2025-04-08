ad84e61eb4f6e73b1d78c2ddd37f908a027f17b8
Fazarama <53871962+fazapoint@users.noreply.github.com>
2025-04-09 00:44:11 +0700

    finished MyAditionCalculator

---
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// print out whatever is inside of ()
+Console.WriteLine("input some text:");
+
+// takes the user input and stores it
+string userInput = Console.ReadLine();
+
+// adding to strings together with concatenation and escape character (\) for double quotes
+Console.WriteLine("You entered \"" + userInput + "\"");
+
+// keep console running (waiting a key to be pressed)
+Console.ReadKey();
