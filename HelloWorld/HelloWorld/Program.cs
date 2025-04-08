a44db532c8e92c19334f59af3a008d4ec3c565cb
Fazarama <53871962+fazapoint@users.noreply.github.com>
2025-04-09 00:04:09 +0700

    finished helloworld project

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Good Morning");
+﻿// declare a (string) variable
+string myFriendsName;
+
+// assign a value to the myFriendsName variable
+myFriendsName = "Gendut";
+
+// use the variable
+Console.WriteLine(myFriendsName);
+
+// overwriting the variable value
+myFriendsName = "kentung";
+Console.WriteLine(myFriendsName);
+
+// declare a variable and assign a value to it
+string myFriendsName2 = "Agan";
+Console.WriteLine(myFriendsName2);
+
+// keep app running (not automatically stopped)
+Console.ReadKey();
+
